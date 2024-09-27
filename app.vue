18213fd60befac7dafdc5a08038824f1614b7578
Vincent Waller <vrwaller@gmail.com>
2024-09-27 14:47:28 -0700

    Layouts

--- app.vue
+++ app.vue
@@ -1,6 +1,31 @@
+<script setup>
+const colorMode = useColorMode()
+
+const color = computed(() => colorMode.value === 'dark' ? '#111827' : 'white')
+
+useHead({
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { key: 'theme-color', name: 'theme-color', content: color },
+  ],
+  htmlAttrs: { lang: 'en' },
+  link: [{ rel: 'icon', href: '/icon.png' }],
+
+})
+
+useSeoMeta({
+  title: 'Dog Walkers',
+  description: 'Dog Walkers is a platform to manage dog walking services.',
+
+})
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+
+  <UNotifications />
+  <UModals />
 </template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const { loggedIn } = useUserSession()
+</script>
+
+<template>
+  <div class="flex flex-col h-screen w-full overflow-hidden">
+    <AppHeader />
+
+    <UMain class="h-full flex">
+      <div
+        v-if="loggedIn"
+        class="w-1/6 h-full p-8 border-r border-gray-200 dark:border-gray-800 flex flex-col space-y-2 "
+      >
+        <ULink
+          to="/schedule"
+          active-class="text-primary"
+        >
+          Sechdule
+        </ULink>
+
+        <ULink
+          to="/manager"
+          active-class="text-primary"
+        >
+          Manage Walkers
+        </ULink>
+      </div>
+
+      <slot />
+    </UMain>
+  </div>
+</template>
